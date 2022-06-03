a3b092f1936c8ef445838f9d6be2206da07eceb7
Jeremy DeBry Jones <j.jones1232@gmail.com>
2022-06-03 14:28:33 -0600

    Revert "Reorganize files (#31)"
    This reverts commit 42aeae1572b9b46246dd6b9f632c3310c9a80d65.

--- src/loot.cpp
+++ src/loot.cpp
@@ -23,22 +23,32 @@
  * You should have received a copy of the GNU General Public License
  * along with Loot Generator.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "loot.hpp"
 
-Loot::Loot(float value)
-{
-    _value = value;
-}
+#ifndef LOOT_H
+#define LOOT_H
+
+#include "loot.hpp"
 
 Loot::Loot(std::string name)
 {
-    _name = name;
+    setName(name);
 }
 
-Loot::Loot(std::string name, float value)
+bool Loot::isInt(float num)
+{
+    if (rintf(num) - num <= 0.0000001)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Loot::setName(std::string name)
 {
     _name = name;
-    _value = value;
 }
 
 std::string Loot::getName()
@@ -51,3 +61,103 @@ float Loot::getValue()
     return _value;
 }
 
+Coin::Coin(std::string name, float value)
+    : Loot(name)
+    {
+        setValue(value);
+    };
+
+void Coin::setValue(float value)
+{
+    if (value >= 0)
+    {
+        _value = value;
+    }
+    else
+    {
+        _value = 0;
+    }
+}
+
+std::string Coin::toString()
+{
+    std::ostringstream oss;
+    std::string pluralityOfCoin = getValue() == 1 ? "coin" : "coins";
+    oss << getValue() << " " << getName() << " " << pluralityOfCoin;
+    return oss.str();
+}
+
+Gemstone::Gemstone(std::string name, float value)
+    : Loot(name)
+{
+    setValue(value);
+}
+
+void Gemstone::setValue(float value)
+{
+    if (value >= 0)
+    {
+        _value = round(value);
+    }
+    else
+    {
+        _value = 0;
+    }
+}
+
+std::string Gemstone::toString()
+{
+    std::ostringstream oss;
+    oss << getName() << " gem worth " << getValue() << " gp";
+    return oss.str();
+}
+
+Art::Art(std::string name, float value)
+    : Loot(name)
+{
+    setValue(value);
+}
+
+void Art::setValue(float value)
+{
+    if (value >= 0)
+    {
+        _value = round(value);
+    }
+    else
+    {
+        _value = 0;
+    }
+}
+
+std::string Art::toString()
+{
+    std::ostringstream oss;
+    oss << getName() << " art worth " << getValue() << " gp";
+    return oss.str();
+}
+
+MagicItem::MagicItem(std::string name, float value)
+    : Loot(name)
+{
+    setValue(value);
+}
+
+void MagicItem::setValue(float value)
+{
+    if (value >= 0)
+    {
+        _value = round(value);
+    }
+    else
+    {
+        _value = 0;
+    }
+}
+
+std::string MagicItem::toString()
+{
+    return getName();
+}
+
+#endif
